889832be3d3edf607e54fab0e6268ecb270b9264
TylerSelden <tyler.selden@salesianum.org>
2023-11-30 08:21:36 -0500

    unstable

---
+++ frontend/admin/script.js
@@ -0,0 +1,56 @@
+var socket;
+
+// todo:
+// edit onmessage
+// handle adminInit
+
+function initSocket(username, password) {
+  socket = new WebSocket("wss://server.benti.dev:8443");
+  // socket = new WebSocket("ws://localhost:8080");
+
+  socket.onmessage = function(event) {
+    var msg = JSON.parse(event.data);
+    if (msg.type == "error") {
+      socket.close();
+      error(msg.message);
+      setTimeout(() => { window.location.reload() }, 5000);
+    }
+
+    var panelContent = document.getElementById("panel-content");
+    // var atBottom = (Math.ceil(panelContent.scrollTop) >= panelContent.scrollHeight - panelContent.offsetHeight - 1);
+    // safe to assume message at this point
+    panelContent.innerHTML += msg.message;
+
+    // if user is already scrolled to bottom, scroll to bottom
+    if (atBottom) panelContent.scrollTop = panelContent.scrollHeight;
+  }
+
+  socket.onopen = function() {
+    socket.send(JSON.stringify({type: "adminInit", username: username, password: password}));
+  }
+
+  socket.onclose = function() {
+    error("Connection closed.");
+    setTimeout(() => { window.location.reload() }, 5000);
+  }
+
+  socket.onerror = function() {
+    socket.close();
+    error("An error occurred.");
+    setTimeout(() => { window.location.reload() }, 5000);
+  }
+}
+
+function sendMessage() {
+  // ensure message isn't too long
+  var msg = {
+    type: "adminMessage",
+    msg: document.getElementById("message-input").value
+  };
+  socket.send(JSON.stringify(msg));
+  // enter key delay
+  setTimeout(() => {
+    document.getElementById("message-input").value = "";
+    document.getElementById("message-input").focus();
+  }, 10);
+}
---
+++ frontend/admin/ui.js
@@ -0,0 +1,80 @@
+function showPanel() {
+  var roomInfoDiv = document.getElementById('room-info');
+  var panelContainerDiv = document.getElementById('panel-container');
+
+  var username = document.getElementById('username').value;
+  var password = document.getElementById('password').value;
+
+  // write username and password to localstorage
+  localStorage.setItem('adminUsername', username);
+  localStorage.setItem('adminPassword', password);
+
+  roomInfoDiv.style.display = 'none';
+  panelContainerDiv.style.display = 'block';
+
+  document.getElementById('room-code-display').innerHTML = "Admin Panel";
+  var username = document.getElementById('username').value;
+  var password = document.getElementById('password').value;
+  initSocket(password, username);
+}
+
+function menuOnEnter(event) {
+  if (event.keyCode == 13) document.getElementById("join-button").click();
+}
+function msgOnEnter(event) {
+  if (event.keyCode == 13 && !event.shiftKey) document.getElementById("send-button").click();
+}
+
+window.onload = function() {
+  document.getElementById("username").addEventListener("keydown", menuOnEnter);
+  document.getElementById("password").addEventListener("keydown", menuOnEnter);
+
+  // yes I stole this from stack overflow
+  if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+    localStorage.setItem('darkMode', 'true');
+  }
+
+  // presets
+
+  // GET parameter stuff
+  params = new URL(document.location).searchParams;
+  if (params.get("username")) {
+    document.getElementById("username").value = params.get("username");
+  }
+  // get username and password from localstorage if exists
+  if (localStorage.getItem('adminUsername')) document.getElementById("username").value = localStorage.getItem('adminUsername');
+  if (localStorage.getItem('adminPassword')) document.getElementById("password").value = localStorage.getItem('adminPassword');
+}
+
+var isDarkMode = localStorage.getItem('darkMode') == 'true';
+
+if (isDarkMode) {
+  document.body.classList.add('dark-mode');
+}
+
+var darkModeToggle = document.getElementById('dark-mode-toggle');
+var body = document.body;
+
+darkModeToggle.addEventListener('click', () => {
+  body.classList.toggle('dark-mode');
+  localStorage.setItem('darkMode', body.classList.contains('dark-mode'));
+});
+
+function alert(msg) {
+  var alert = document.getElementsByClassName('alert')[0];
+  alert.style.backgroundColor = '#00c3e6';
+  alert.getElementsByTagName('h3')[0].innerHTML = msg;
+  showAlertElem();
+}
+function error(msg) {
+  var alert = document.getElementsByClassName('alert')[0];
+  alert.style.backgroundColor = '#c50000';
+  alert.getElementsByTagName('h3')[0].innerHTML = msg;
+  showAlertElem();
+}
+// show alert div
+function showAlertElem() {
+  var alert = document.getElementsByClassName('alert')[0];
+  alert.style.transform = 'translateY(0)';
+  setTimeout(() => { alert.style.transform = 'translateY(-150%)'; }, 5000);
+}
