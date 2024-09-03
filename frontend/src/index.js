b63ed0ae79be6ccf896fd377c3d7babd5ebdc7c4
TylerSelden <tyler.selden@salesianum.org>
2024-09-03 14:01:13 +0000

    init refactor

---
+++ frontend/src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./style.css"
+
+//import Main from "./components/main";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
+    <div>
+      <p>This page is under maintenence. Sorry for the inconvenience. (inconvinience? inconveneince? whatever you get the idea)</p>
+    </div>
+)
